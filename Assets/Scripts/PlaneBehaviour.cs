07a336e7d0f3802f2e7a3be826888512d22c1a5e
SanazSobhanloo <cnzsg1@gmail.com>
2026-02-02 11:12:26 +0330

    feat: enhance PlaneBehaviour with game over handling and UI updates

--- Assets/Scripts/PlaneBehaviour.cs
+++ Assets/Scripts/PlaneBehaviour.cs
@@ -2,86 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaneBehaviour : MonoBehaviour
-{
+public class PlaneBehaviour : MonoBehaviour {
 
 	public Camera cam;
 	public Transform plane;
-	public GameObject brokenPlane, canvas;
+	public GameObject brokenPlane, GameOverPanel;
+	public GameObject inGameUI;
 	public Transform leftPoint, rightPoint, forwardPoint;
 	Rigidbody2D rb;
 	public float speed = 5f, rotateSpeed = 50f;
 	Vector3 mousePosition;
 	bool isGameOver;
 
+
 	// Use this for initialization
-	void Start()
-	{
-		if (cam == null) cam = Camera.main;
+	void Start () {
+		if(cam == null) cam = Camera.main;
 		rb = GetComponent<Rigidbody2D>();
 	}
-
+	
 	// Update is called once per frame
-	void FixedUpdate()
-	{
-		if (!isGameOver)
-		{
+	void FixedUpdate () {
+		if(!isGameOver){
 			movePlane();
 			rotatePlane(Input.GetAxis("Horizontal"));
 		}
 	}
 
-	void movePlane()
-	{
-		rb.velocity = transform.up * speed;
+	void movePlane(){
+		rb.linearVelocity = transform.up * speed;
 	}
 
-	void rotatePlane(float x)
-	{
+	void rotatePlane(float x){	
 		float angle;
-		Vector2 direction = new Vector2(0, 0);
+		Vector2 direction = new Vector2(0,0);
 
-		if (x < 0) direction = (Vector2)leftPoint.position - rb.position;
-		if (x > 0) direction = (Vector2)rightPoint.position - rb.position;
-
+		if(x < 0) direction = (Vector2) leftPoint.position - rb.position;
+		if(x > 0) direction = (Vector2) rightPoint.position - rb.position;
+		
 		direction.Normalize();
 		angle = Vector3.Cross(direction, transform.up).z;
-		if (x != 0) rb.angularVelocity = -rotateSpeed * angle;
-		else rb.angularVelocity = 0;
+		if(x != 0) rb.angularVelocity = -rotateSpeed * angle;
+		else rb . angularVelocity = 0;
 		angle = Mathf.Atan2(forwardPoint.position.y - plane.transform.position.y, forwardPoint.position.x - plane.transform.position.x) * Mathf.Rad2Deg;
-		plane.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+		plane.transform.rotation = Quaternion.Euler(new Vector3(0,0,angle));
 	}
 
 	public void gameOver(Transform missile)
-	{
-		GetComponentInChildren<SpriteRenderer>().enabled = false;
-		isGameOver = true;
-		rb.velocity = new Vector2(0, 0);
-		cam.gameObject.GetComponent<CameraBehaviour>().gameOver = true;
-		GameObject planeTemp = Instantiate(brokenPlane, transform.position, transform.rotation);
-		for (int i = 0; i < planeTemp.transform.childCount; i++)
-		{
-			Rigidbody2D rbTemp = planeTemp.transform.GetChild(i).gameObject.GetComponent<Rigidbody2D>();
-			rbTemp.AddForce(((Vector2)missile.position - rbTemp.position) * -5f, ForceMode2D.Impulse);
-		}
-		StartCoroutine(canvasStuff());
+{
+    // جلوگیری از دوبار اجرا شدن گیم اور
+    if (isGameOver) return;
 
-		// New: inform GameManager (safe: only if exists)
-		if (GameManager.Instance != null)
-		{
-			GameManager.Instance.HandlePlayerDeath();
-		}
-		
-	}
+    GetComponentInChildren<SpriteRenderer>().enabled = false;
+    isGameOver = true;
 
-	IEnumerator canvasStuff()
-	{
+    rb.linearVelocity = Vector2.zero;
+
+    if (cam != null)
+        cam.gameObject.GetComponent<CameraBehaviour>().gameOver = true;
+
+    GameObject planeTemp = Instantiate(brokenPlane, transform.position, transform.rotation);
+    for (int i = 0; i < planeTemp.transform.childCount; i++)
+    {
+        Rigidbody2D rbTemp = planeTemp.transform.GetChild(i).GetComponent<Rigidbody2D>();
+        if (rbTemp != null)
+            rbTemp.AddForce(((Vector2)missile.position - rbTemp.position) * -5f, ForceMode2D.Impulse);
+    }
+
+    // امتیاز رو ذخیره کن
+    if (ScoreManager.Instance != null)
+        ScoreManager.Instance.StopAndSaveIfHighScore();
+
+    // ✅ UI: وسط بازی خاموش، گیم اور روشن
+    if (inGameUI != null) inGameUI.SetActive(false);
+    if (GameOverPanel != null) GameOverPanel.SetActive(true);
+
+    // اگر دیگه لازم نداری، اینو حذف کن.
+    // اگر canvasStuff کار دیگه‌ای می‌کنه (مثل انیمیشن/تاخیر)، می‌تونی نگهش داری.
+    // StartCoroutine(canvasStuff());
+}
+
+
+	IEnumerator canvasStuff(){
 		yield return new WaitForSeconds(1f);
-		canvas.SetActive(true);
-		for (int i = 0; i <= 10; i++)
-		{
-			float k = (float)i / 10;
-			canvas.transform.localScale = new Vector3(k, k, k);
+		GameOverPanel.SetActive(true);
+		for(int i = 0; i <= 10; i++){
+			float k = (float) i /10;
+			GameOverPanel.transform.localScale = new Vector3(k,k,k);
 			yield return new WaitForSeconds(.01f);
 		}
 		Destroy(gameObject);
